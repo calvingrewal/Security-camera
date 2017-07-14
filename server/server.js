ab5533afb3c991d98680560bead93583b01da476
Calvin Grewal <calvingrewal28@gmail.com>
2017-07-13 22:55:43 -0700

    setup server side socket handling

--- server/server.js
+++ server/server.js
@@ -1,13 +1,115 @@
 const express = require('express')
+const http = require('http');
 const path = require('path')
 
 const app = express()
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.use(express.static(path.join(__dirname, '../static')))
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../static/index.html'))
 })
 
-app.listen(3000, () => {
-  console.log('****************listening on port 3000****************');
-})
+const users = {}
+
+io.on('connection', (socket) => {
+  console.log('user connected')
+
+  socket.on('message', (message) => {
+    console.log('received message', message)
+    const data = JSON.parse(message)
+    const conn = users[data.name];
+
+    switch (data.type) {
+      case "login":
+        console.log(data.name, 'logging in');
+
+        if(users[data.name]) {
+          socket.send(JSON.stringify({
+            type: "login",
+            success: false
+          }));
+        } else {
+          users[data.name] = socket;
+          socket.name = data.name;
+
+          socket.send(JSON.stringify({
+            type: "login",
+            success: true
+          }))
+        }
+        break;
+      case "offer":
+        console.log("Sending offer to: ", data.name);
+
+
+        if(conn != null) {
+          socket.otherName = data.name;
+
+          socket.send(JSON.stringify({
+            type: "offer",
+            offer: data.offer,
+            name: socket.name
+          }))
+        }
+        break;
+      case "answer":
+        console.log("Sending answer to: ", data.name);
+
+        if(conn != null) {
+          socket.otherName = data.name;
+          socket.send(JSON.stringify({
+            type: "answer",
+            answer: data.answer
+          }))
+        }
+        break;
+      case "candidate":
+        console.log("Sending candidate to:",data.name);
+
+        if(conn != null) {
+          socket.send(JSON.stringify({
+            type: "candidate",
+            candidate: data.candidate
+          }))
+        }
+        break;
+      case "leave":
+        console.log("Disconnecting from", data.name);
+        conn.otherName = null;
+
+        if(conn != null) {
+          socket.send(JSON.stringify({
+            type: "leave"
+          }))
+        }
+
+        break;
+    }
+  })
+  socket.on("close", function() {
+
+    if(socket.name) {
+      delete users[socket.name];
+
+      if(socket.otherName) {
+        console.log("Disconnecting from ", socket.otherName);
+        const conn = users[socket.otherName];
+        conn.otherName = null;
+
+        if(conn != null) {
+          socket.send(JSON.stringify({
+             type: "leave"
+          }))
+        }
+      }
+    }
+  });
+
+  socket.send('Hello from server')
+
+});
+
+server.listen(3000);
